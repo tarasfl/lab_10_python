268b15c62ea7e0853982183fdce939a38d707d5a
tarasfl <tfliorko@gmail.com>
2022-06-02 00:26:23 +0300

    f

--- lab_10.py
+++ lab_10.py
@@ -1,66 +1,74 @@
 class Node:
-    def __init__(self, type_of_transistor, mark, i_k_max, u_k_max):
+    def __init__(self, key):
         self.right = None
         self.left = None
-        self.type_of_transistor = type_of_transistor
-        self.mark = mark
-        self.i_k_max = i_k_max
-        self.u_k_max = u_k_max
+        self.key = key
+        self.i_k_max = None
 
-    def add_element(self, type_of_transistor, mark, i_k_max, u_k_max):
-        if self.i_k_max:
-            if i_k_max < self.i_k_max:
+    def add_element_by_key(self, key):
+        if self.key:
+            if key < self.key:
                 if self.left is None:
-                    self.left = Node(type_of_transistor, mark, i_k_max, u_k_max)
-                    print("creating Node Left", self.i_k_max)
+                    self.left = Node(key)
+                    return self.left
                 else:
-                    self.left.add_element(type_of_transistor, mark, i_k_max, u_k_max)
-            elif i_k_max > self.i_k_max:
+                    self.left.add_element_by_key(key)
+            elif key > self.key:
                 if self.right is None:
-                    self.right = Node(type_of_transistor, mark, i_k_max, u_k_max)
-                    print("creating Node Right", self.i_k_max)
+                    self.right = Node(key)
+                    return self.right
                 else:
-                    self.right.add_element(type_of_transistor, mark, i_k_max, u_k_max)
+                    self.right.add_element_by_key(key)
         else:
-            self.i_k_max = i_k_max
-            self.u_k_max = u_k_max
-            self.mark = mark
-            self.type_of_transistor = type_of_transistor
+            self.key = key
+            return self
 
-    def delete_element(self, type_of_transistor, mark, i_k_max, u_k_max):
-        if self.i_k_max:
-            if i_k_max > self.i_k_max:
-                if self.right is None:
-                    print("There is no such element")
-                else:
-                    print("reference right")
-                    self.right.delete_element(type_of_transistor, mark, i_k_max, u_k_max)
-            elif i_k_max < self.i_k_max:
-                if self.left is None:
-                    print("There is no such element")
-                else:
-                    print("reference left")
-                    self.left.delete_element(type_of_transistor, mark, i_k_max, u_k_max)
-            elif self.i_k_max == i_k_max and self.u_k_max == u_k_max and self.mark == mark \
-                    and self.type_of_transistor == type_of_transistor:
-                if self.right:
-                    self.right = self.left
-                    del self
-                elif self.left:
-                    self.left = self.right
+    def add_element_with_i_k_max(self, key, i_k_max):
+        element = self.add_element_by_key(key)
+
+    def delete_element(self, key):
+        element = self.find_by_key(key)
+        if element is not None:
+            if element.right is not None:
+                current_element = element.right
+                while current_element.left is not None:
+                    current_element = current_element.left
+                element.key = current_element.key
+                if current_element.right is not None and element is not self:
+                    current_element.replace(current_element.right)
+            else:
+                if element.left is not None:
+                    element.replace(element.left)
+        else:
+            print("there is no such element")
 
     def print_all_elements(self):
         if self.left:
             self.left.print_all_elements()
-        print(self.i_k_max, "  ", self.left, "  ", self.right),
+        print(self.key, " ", self.left, '  ', self.right)
         if self.right:
             self.right.print_all_elements()
 
     def find_by_key(self, key):
-        pass
+        if self.key:
+            if key == self.key:
+                return self
+            elif key < self.key:
+                if self.left is None:
+                    return None
+                else:
+                    return self.left.find_by_key(key)
+            elif key > self.key:
+                if self.right is None:
+                    return None
+                else:
+                    return self.right.find_by_key(key)
+        else:
+            return None
 
-    def get_data_of_element(self):
-        pass
+    def get_data_of_element(self, key):
+        element = self.find_by_key(key)
+        return element.key, "  ", element.i_k_max
 
     def bypass_tree(self):
         pass
@@ -68,14 +76,27 @@ class Node:
     def delete_all_elements_by_type(self, type_of_transistor):
         pass
 
+    def replace(self, obj):
+        self.key = obj.key
+        if self.right is not None:
+            self.right = obj.right
+        elif self.left is not None:
+            self.left = obj.left
+        del obj
+
 
 if __name__ == '__main__':
-    root = Node("p-n-p", "ka-12", 20, 28)
-    root.add_element("p-n-p", "kra-12", 54, 280)
-    root.add_element("p-n-p", "kra-12", 44, 280)
-    root.add_element("p-n-p", "kra-12", 14, 280)
-    root.add_element("p-n-p", "kra-12", 104, 280)
+    root = Node(20)
+    root.add_element_by_key(10)
+    root.add_element_by_key(40)
+    root.add_element_by_key(30)
+    root.add_element_by_key(35)
+    root.add_element_by_key(34)
+    root.add_element_by_key(33)
+    root.add_element_by_key(31)
+    root.add_element_by_key(32)
     root.print_all_elements()
-    root.delete_element("p-n-p", "kra-12", 44, 280)
-    print("-------------")
+    print(root.delete_element(20))
+    print("-----------")
     root.print_all_elements()
+   # print(root.find_by_key(30).right.key)
